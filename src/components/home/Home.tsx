c333f9041e6455d78d07bd0767f372709dfbd583
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-10-23 01:17:40 -0700

    barebones website

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>TritonCook</h1>
+      <p>Et ullamco consectetur magna ex enim consequat irure deserunt culpa aliqua pariatur amet duis eu. Dolore labore ex exercitation proident aute cillum laborum anim exercitation nisi Lorem magna. Esse Lorem magna cupidatat anim anim nostrud occaecat ad pariatur nostrud occaecat in exercitation. Veniam eiusmod occaecat occaecat qui occaecat mollit. In esse reprehenderit quis officia in. Culpa elit sunt elit ipsum esse irure sunt.</p>
+    </div>
+  );
+};
+
+export default Home;
