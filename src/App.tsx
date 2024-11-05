efa62cafd64f45bc014b4403fb6040c9efc559d7
adutta4 <duttaaditi37@gmail.com>
2024-11-05 13:55:34 -0800

    changed app structure to add NewsFeed view

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from './components/home/Home';
-import RecipeList from "./components/recipes/RecipeList";
-import RecipeDetail from './components/recipes/RecipeDetail';
+import RecipeList from "./components/recipes/DummyRecipeList";
+import RecipeDetail from './components/recipes/DummyRecipeDetail';
+import { NewsFeed } from './views/NewsFeed';
 import './App.css';
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<RecipeList />} />
+        <Route path="/recipes" element={<NewsFeed />} />
         <Route path="/recipes/:id" element={<RecipeDetail />} />
       </Routes>
     </div>
---
+++ src/views/NewsFeed.tsx
@@ -0,0 +1,9 @@
+import RecipeList from "../components/recipes/DummyRecipeList";
+import RecipeDetail from "../components/recipes/DummyRecipeDetail";
+
+export const NewsFeed = () => {
+    return (
+    <div>
+        <RecipeList/>
+    </div>)
+}
