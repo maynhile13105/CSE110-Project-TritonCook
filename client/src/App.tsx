66c4291565957d951a12b06e881b09495bd37ba6
maynhile13105 <maynhile13105@gmail.com>
2024-11-07 11:52:53 -0800

    Loginhomepage done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/header/Header";
-import Home from './components/home/Home';
-import RecipeList from "./components/recipes/RecipeList";
-import RecipeDetail from './components/recipes/RecipeDetail';
 import './App.css';
-
-const App = () => {
+import {LoginProvider} from './context/LoginContext';
+import { LoginHomepage } from './views/LoginHomepage';
+function App() {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<RecipeList />} />
-        <Route path="/recipes/:id" element={<RecipeDetail />} />
-      </Routes>
-    </div>
+      <LoginProvider>
+        <LoginHomepage />
+      </LoginProvider>
+    
   );
 }
 
---
+++ client/src/views/LoginHomepage.tsx
@@ -0,0 +1,26 @@
+import LoginForm from "../components/LoginForm";
+import "../css/LoginHomepage.css";
+
+export const LoginHomepage = () => {
+    return (
+        <div className="login-homepage-container">
+            <div className="left-section">
+                <div className="slogan-container">
+                    <h1 className="slogan">"Share, Discover, Savor"</h1>
+                    <h1 className="app-name">TritonCook</h1>
+                </div>
+                <div className="logo-container">
+                    <img src="/logo.png" className="logo" alt="TritonCook Logo" />
+                </div>
+            </div>
+            
+            <div className="right-section">
+                <div className="login-form-container">
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginHomepage;
