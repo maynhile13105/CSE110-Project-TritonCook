25b5706fdb15983d95f251ed1cc332aa4a04e990
maynhile13105 <maynhile13105@gmail.com>
2024-11-07 02:40:37 -0800

    Reset password interface done

--- tritoncook-app/src/App.tsx
+++ tritoncook-app/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import {LoginProvider} from './context/LoginContext';
 import { LoginHomepage } from './views/LoginHomepage';
+import ResetPasswordPage from './views/ResetPasswordPage';
 
 function App() {
   return (
       <LoginProvider>
-        <LoginHomepage />
+        <ResetPasswordPage />
       </LoginProvider>
     
   );
---
+++ tritoncook-app/src/views/ResetPasswordPage.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import "../css/ResetPasswordPage.css";
+
+export const ResetPasswordPage = () => {
+    return(
+        <div className="reset-pass-page">
+            <div>
+                <button className="back-button">
+                    <i className="bi bi-box-arrow-in-left" style={{fontSize:"80px"}}></i>
+                    Back
+                </button>
+            </div>
+
+            <div className="grid-container">
+                <div className="reset-form-container">
+                    <div className="title">
+                        <h1 className="recover-password-header">Recover Password</h1>
+                    </div>
+                    <div className="form-container">
+                        <form className="reset-form form-group"> 
+                            <p className="instruction">
+                                Enter your email address! <br/>
+                                We will send you an email with instructions to reset your password
+                            </p>                            
+
+                            <div data-mdb-input-init className="form-outline">
+                                <label className="email-label">Email</label>
+                                <input type="email" placeholder = "Enter Email" id="userEmail" className="form-control my-3" />
+                            </div>
+
+                            <div className="reset-button">
+                                <button type="submit" id="resetButton">RESET PASSWORD</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+export default ResetPasswordPage;
