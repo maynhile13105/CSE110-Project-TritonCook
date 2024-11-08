c5ff3a190e4cafecbbbea7b54ffd130e6991b05d
yifanzhu1121 <151365496+yifanzhu1121@users.noreply.github.com>
2024-11-08 01:35:15 -0800

    update

--- client/src/Newsfeed/Newsfeed.tsx
+++ client/src/Newsfeed/Newsfeed.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Home from '../components/home/Home';
-import SearchPage from '../components/recipes/SearchPage';
+import SearchPage from '../components/searchpage/SearchPage';
 import { Route, Routes } from "react-router-dom";
 import './Newsfeed.css'
 
