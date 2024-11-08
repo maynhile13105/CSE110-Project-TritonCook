aaecb7d914cb300b3ded0fcffa3e68dca71b7f23
yifanzhu1121 <151365496+yifanzhu1121@users.noreply.github.com>
2024-11-08 01:25:44 -0800

    Update and rename Header.tsx to Navbar.tsx

---
+++ client/src/components/header/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <main className='navbar'>
+        <ul className='left'>
+          <li className='logo'><a href='/'>
+          <img src='/logo-round.svg'/>
+          </a></li>
+          <li className='notif'><a href='/'>
+          <img src='/notif-bell.svg'/>
+          </a></li>
+        </ul>
+        <ul className='middle'>
+          <li className='post'><a href='/'>
+          <img className='postImage' src='/addPost.svg'/>
+          </a></li>
+          <li className='home'><a href='/'>
+          <img src='/home.svg'/>
+          </a></li>
+          <li className='search'><a href='/recipes'>
+          <img src='/search.svg'/>
+          </a></li>
+        </ul>
+        <ul className='right'>
+          <li className='logout'><a href='/'>
+          <img src='/logout.svg'/>
+          </a></li>
+          <li className='profile'><a href='/'>
+          <img src='/profile.svg'/>
+          </a></li>
+
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default Navbar;
