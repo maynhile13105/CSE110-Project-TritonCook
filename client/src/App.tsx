d7c323a7ffb82161b10bbb511093ef200b5f4764
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-07 18:17:19 -0800

    added links to where applicable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
 import {LoginProvider} from './context/LoginContext';
 import { LoginHomepage } from './views/LoginHomepage';
 import ResetPasswordPage from './views/ResetPasswordPage';
 import WelcomePage from './views/WelcomePage';
 function App() {
   return (
-      <LoginProvider>
-        <WelcomePage />
-      </LoginProvider>
+    <div>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginHomepage />} />
+        <Route path="/login/reset" element={<ResetPasswordPage />} />
+      </Routes>
+    </div>
     
   );
 }
--- client/src/views/WelcomePage.tsx
+++ client/src/views/WelcomePage.tsx
@@ -1,25 +1,28 @@
 import "../css/WelcomePage.css"
-export const WelcomePage = () => {
-    return(
-        <div className="welcome-container">
-            <div className="welcome-header">
-                <h1>WELCOME TO TRITON COOK</h1>
-            </div>
-            <div className="logo-container">
-                <img src="/logo.png" className="logo" alt="TritonCook Logo" />
-            </div>
+import { Link } from 'react-router-dom';
 
-            <div className="congrat-message">
-                <p>
-                    Congratulations! <br/>
-                    Your account has been created successfully!
-                </p>
-                <div>
-                    <button type="button" className="login-button"
-                            onClick={()=>window.location.href='#'}>Login</button>
-                </div>
-            </div>
+export const WelcomePage = () => {
+  return (
+    <div className="welcome-container">
+      <div className="welcome-header">
+        <h1>WELCOME TO TRITON COOK</h1>
+      </div>
+      <div className="logo-container">
+        <img src="/logo.png" className="logo" alt="TritonCook Logo" />
+      </div>
+
+      <div className="congrat-message">
+        <p>
+          Congratulations! <br />
+          Your account has been created successfully!
+        </p>
+        <div>
+          <Link to="/login">
+            <button type="button" className="login-button">Login</button>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  )
 };
 export default WelcomePage;
