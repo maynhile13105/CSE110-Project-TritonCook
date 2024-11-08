ac6931dd28aa144ce3033ebb571cecc33602bff6
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-07 18:22:51 -0800

    added LoginProvider to WelcomePage

--- client/src/views/WelcomePage.tsx
+++ client/src/views/WelcomePage.tsx
@@ -1,28 +1,31 @@
+import { LoginProvider } from "../context/LoginContext";
 import "../css/WelcomePage.css"
 import { Link } from 'react-router-dom';
 
 export const WelcomePage = () => {
   return (
-    <div className="welcome-container">
-      <div className="welcome-header">
-        <h1>WELCOME TO TRITON COOK</h1>
-      </div>
-      <div className="logo-container">
-        <img src="/logo.png" className="logo" alt="TritonCook Logo" />
-      </div>
+    <LoginProvider>
+      <div className="welcome-container">
+        <div className="welcome-header">
+          <h1>WELCOME TO TRITON COOK</h1>
+        </div>
+        <div className="logo-container">
+          <img src="/logo.png" className="logo" alt="TritonCook Logo" />
+        </div>
 
-      <div className="congrat-message">
-        <p>
-          Congratulations! <br />
-          Your account has been created successfully!
-        </p>
-        <div>
-          <Link to="/login">
-            <button type="button" className="login-button">Login</button>
-          </Link>
+        <div className="congrat-message">
+          <p>
+            Congratulations! <br />
+            Your account has been created successfully!
+          </p>
+          <div>
+            <Link to="/login">
+              <button type="button" className="login-button">Login</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </LoginProvider>
   )
 };
 export default WelcomePage;
