8389735edd72b57b7cdb6f400bb62806aaa57d22
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-07 18:26:45 -0800

    import linting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import {LoginProvider} from './context/LoginContext';
 import { LoginHomepage } from './views/LoginHomepage';
 import ResetPasswordPage from './views/ResetPasswordPage';
 import WelcomePage from './views/WelcomePage';
