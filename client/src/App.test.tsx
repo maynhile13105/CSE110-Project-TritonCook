c882239af3b7ffb77a6d29405838c64dab2e4fa6
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-10 01:51:43 -0800

    created basic test cases for rendering

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import LoginHomepage from './views/LoginHomepage';
+import ResetPasswordPage from './views/ResetPasswordPage';
+import CreateAccountPage from './views/CreateAccountPage';
+import { MemoryRouter, Routes } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders login page', () => {
+  render(
+    <MemoryRouter >
+      <LoginHomepage />
+    </MemoryRouter >);
+
+  const TritonCook = screen.getByText("TritonCook");
+  expect(TritonCook).toBeInTheDocument();
+
+  const username = screen.getByText("Username");
+  expect(username).toBeInTheDocument();
+
+  const password = screen.getByText("Password");
+  expect(password).toBeInTheDocument();
+});
+
+test('renders recovery page', () => {
+  render(
+    <MemoryRouter >
+      <ResetPasswordPage />
+    </MemoryRouter >);
+
+  const recover = screen.getByText("Recover Password");
+  expect(recover).toBeInTheDocument();
+
+  const email = screen.getByText("Email");
+  expect(email).toBeInTheDocument();
+});
+
+test('renders account creation page', () => {
+  render(
+    <MemoryRouter >
+      <CreateAccountPage />
+    </MemoryRouter>);
+    
+  const CreateAccount = screen.getByText("Create Account");
+  expect(CreateAccount).toBeInTheDocument();
+
+  const username = screen.getByText("Username");
+  expect(username).toBeInTheDocument();
+
+  const email = screen.getByText("Email");
+  expect(email).toBeInTheDocument();
+
+  const password = screen.getByText("Password");
+  expect(password).toBeInTheDocument();
+
+  const confirmPassword = screen.getByText("Confirm Password");
+  expect(confirmPassword).toBeInTheDocument();
 });
--- client/src/components/loginForm/LoginForm.tsx
+++ client/src/components/loginForm/LoginForm.tsx
@@ -1,9 +1,20 @@
+global.google = {
+  accounts: {
+      id: {
+          initialize: jest.fn(),
+          prompt: jest.fn(),
+          renderButton: jest.fn(),
+      },
+  },
+};
+
 import React, { useEffect, useState } from "react";
 import "../../css/LoginForm.css";
 import { Link } from "react-router-dom";
 import { GoogleCredentialResponse } from "../../types/types";
 
 const LoginForm = () => {
+  
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
   useEffect(() => {
