4497065310597e4be3f3fb6b60794771531553a9
ribaltazar <ribaltazar@ucsd.edu>
2024-11-10 21:30:39 -0800

    Navbar 'render' test cases. Routes added to newsfeed

--- client/src/Newsfeed/Newsfeed.tsx
+++ client/src/Newsfeed/Newsfeed.tsx
@@ -3,14 +3,18 @@ import Navbar from '../components/Navbar/Navbar';
 import Home from '../components/home/Home';
 import RecipeList from '../components/recipes/RecipeList';
 import RecipeDetail from '../components/recipes/RecipeDetail';
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import './Newsfeed.css'
 
 const Newsfeed = () => {
     return(
         <div>
             <Navbar />
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/recipes" element={<RecipeList />} />
+                <Route path="/recipes/:id" element={<RecipeDetail />} />
+            </Routes>
         </div>
     );
 }
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,37 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
+import RecipeList from '../recipes/RecipeList';
 
 const Navbar = () => {
   return (
     <div>
       <main className='navbar'>
         <ul className='left'>
-          <li className='logo'><a href='/'>
-          <img src='/logo-round.svg'/>
-          </a></li>
-          <li className='notif'><a href='/'>
-          <img src='/notif-bell.svg'/>
-          </a></li>
+          <li className='logo'><Link to='/'>
+          <img src='/logo-round.svg'
+          alt='logo'/>
+          </Link></li>
+          <li className='notif'><Link to='/'>
+          <img src='/notif-bell.svg'
+          alt='notif'/>
+          </Link></li>
         </ul>
         <ul className='middle'>
-          <li className='post'><a href='/'>
-          <img className='postImage' src='/addPost.svg'/>
-          </a></li>
-          <li className='home'><a href='/'>
-          <img src='/home.svg'/>
-          </a></li>
-          <li className='search'><a href='/recipes'>
-          <img src='/search.svg'/>
-          </a></li>
+          <li className='post'><Link to='/'>
+          <img className='postImage' src='/addPost.svg' alt ='post' />
+          </Link></li>
+          <li className='home'><Link to='/'>
+          <img src='/home.svg' alt='home' />
+          </Link></li>
+          <li className='search'><Link to='/recipes'>
+          <img src='/search.svg' alt='search'/>
+          </Link></li>
         </ul>
         <ul className='right'>
-          <li className='logout'><a href='/'>
-          <img src='/logout.svg'/>
-          </a></li>
-          <li className='profile'><a href='/'>
-          <img src='/profile.svg'/>
-          </a></li>
-
+          <li className='logout'><Link to='/'>
+          <img src='/logout.svg' alt='logout'/>
+          </Link></li>
+          <li className='profile'><Link to='/'>
+          <img src='/profile.svg' alt='profile'/>
+          </Link></li>
         </ul>
       </main>
     </div>
