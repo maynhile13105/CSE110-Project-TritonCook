2f4bc812d922ce0e783f84099b56ac77053f2ea4
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-12 20:34:51 -0800

    clean up imports

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -8,31 +8,31 @@ const Navbar = () => {
       <main className='navbar'>
         <ul className='left'>
           <li className='logo'><Link to='/home'>
-          <img src='images/logo-round.svg'
-          alt='logo'/>
+            <img src='images/logo-round.svg'
+              alt='logo' />
           </Link></li>
           <li className='notif'><Link to='/home'>
-          <img src='images/notif-bell.svg'
-          alt='notif'/>
+            <img src='images/notif-bell.svg'
+              alt='notif' />
           </Link></li>
         </ul>
         <ul className='middle'>
           <li className='post'><Link to='/home'>
-          <img className='postImage' src='images/addPost.svg' alt ='post' />
+            <img className='postImage' src='images/addPost.svg' alt='post' />
           </Link></li>
           <li className='home'><Link to='/home'>
-          <img src='images/home.svg' alt='home' />
+            <img src='images/home.svg' alt='home' />
           </Link></li>
           <li className='search'><Link to='/home/recipes'>
-          <img src='images/search.svg' alt='search'/>
+            <img src='images/search.svg' alt='search' />
           </Link></li>
         </ul>
         <ul className='right'>
           <li className='logout'><Link to='/home'>
-          <img src='images/logout.svg' alt='logout'/>
+            <img src='images/logout.svg' alt='logout' />
           </Link></li>
           <li className='profile'><Link to='/account'>
-          <img src='images/profile.svg' alt='profile'/>
+            <img src='images/profile.svg' alt='profile' />
           </Link></li>
         </ul>
       </main>
