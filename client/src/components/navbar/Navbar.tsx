2530ca42713e0dbb3b873653c20d2b5125dcec56
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-13 22:12:07 -0800

    fixed search link on navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           <li className='home'><Link to='/home'>
           <img src='images/home.svg' alt='home' />
           </Link></li>
-          <li className='search'><Link to='/home/recipes'>
+          <li className='search'><Link to='/home/search'>
           <img src='images/search.svg' alt='search'/>
           </Link></li>
         </ul>
