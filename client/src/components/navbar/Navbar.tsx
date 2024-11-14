ea9d95652385bf7b813ba05f4803ae45ed265c2c
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-14 05:15:29 -0800

    added support for getting user icon from server

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './Navbar.css';
+import UserIcon from '../google/UserIcon';
 
 const Navbar = () => {
   return (
@@ -8,31 +9,31 @@ const Navbar = () => {
       <main className='navbar'>
         <ul className='left'>
           <li className='logo'><Link to='/home'>
-          <img src='images/logo-round.svg'
-          alt='logo'/>
+            <img src='images/logo-round.svg'
+              alt='logo' />
           </Link></li>
           <li className='notif'><Link to='/home'>
-          <img className='notifImage' src='images/notif-bell.svg'
-          alt='notif'/>
+            <img className='notifImage' src='images/notif-bell.svg'
+              alt='notif' />
           </Link></li>
         </ul>
         <ul className='middle'>
           <li className='post'><Link to='/home'>
-          <img className='postImage' src='images/addPost.svg' alt ='post' />
+            <img className='postImage' src='images/addPost.svg' alt='post' />
           </Link></li>
           <li className='home'><Link to='/home'>
-          <img src='images/home.svg' alt='home' />
+            <img src='images/home.svg' alt='home' />
           </Link></li>
           <li className='search'><Link to='/home/recipes'>
-          <img src='images/search.svg' alt='search'/>
+            <img src='images/search.svg' alt='search' />
           </Link></li>
         </ul>
         <ul className='right'>
           <li className='logout'><Link to='/home'>
-          <img src='images/logout.svg' alt='logout'/>
+            <img src='images/logout.svg' alt='logout' />
           </Link></li>
           <li className='profile'><Link to='/home'>
-          <img src='images/profile.svg' alt='profile'/>
+            <UserIcon />
           </Link></li>
         </ul>
       </main>
