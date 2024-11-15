e5d869460abcc643610d80b9b936ef6a3ef98bbb
adutta4 <duttaaditi37@gmail.com>
2024-11-15 00:52:30 -0800

    fix like functionality

--- client/src/components/recipes/RecipeItem.tsx
+++ client/src/components/recipes/RecipeItem.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Recipe } from "../../types/types";
 import { RecipeContext } from "../../context/RecipeContext";
 import './RecipeItem.css'
@@ -6,6 +6,22 @@ import { Link } from "react-router-dom";
 
 const RecipeItem = (currentRecipe: Recipe) => {
   const { recipes, setRecipes } = useContext(RecipeContext);
+  const { favorites, setFavorites } = useContext(RecipeContext);
+
+  const handleFavoriteClick = (id: number) => {
+    const updatedRecipes = recipes.map((recipe) =>
+      recipe.id === id ? { ...recipe, favorite: !recipe.favorite } : recipe
+    );
+
+    console.log("Updated Recipes:", updatedRecipes);
+
+    setRecipes([...updatedRecipes]);
+    setFavorites([...favorites, currentRecipe]);
+  }
+
+  useEffect(() => {
+    console.log("Rendering RecipeItem with recipe:", currentRecipe);
+  }, [currentRecipe.favorite]); 
 
   return (
     <div className="post-box">
@@ -14,7 +30,10 @@ const RecipeItem = (currentRecipe: Recipe) => {
           <img src='/profile.svg' />
           Username
         </div>
-        <img src='/like-unliked.svg' />
+        <button className='like-button' onClick={() => handleFavoriteClick(currentRecipe.id)}>
+          <img src={!currentRecipe.favorite ? "/like-unliked.svg" : "/Heart.svg"} alt="Button Image" />
+        </button>
+        {/* <img src='' /> */}
       </div>
       <br />
       <div className="post-name">{currentRecipe.name}</div>
