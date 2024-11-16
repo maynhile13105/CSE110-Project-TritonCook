ea7065812ac4b68ad795b3cb4c8cd6bf3a78233c
ribaltazar <ribaltazar@ucsd.edu>
2024-11-15 17:30:51 -0800

    sidebar update

--- client/src/components/navbar/Sidebar.tsx
+++ client/src/components/navbar/Sidebar.tsx
@@ -5,8 +5,10 @@ import './Sidebar.css'
 const Sidebar = () => {
   const[dropdown, setDropdown] = useState(false);
   const[timePopup, setTimePopup] = useState(false);
+  const[ingredientsPopup, setIngredientsPopup] = useState(false);
+  const[cuisinesPopup, setCuisinesPopup] = useState(false);
   const[selectedTime, setSelectedTime] = useState<string | null>(null);
-
+  const[selectedCuisine, setSelectedCuisine] = useState<string | null>(null);
   const toggleDropdown = () => {
     setDropdown(!dropdown);
   };
@@ -15,6 +17,14 @@ const Sidebar = () => {
     setTimePopup(!timePopup);
   };
 
+  const toggleIngredientsPopup = () => {
+    setIngredientsPopup(!ingredientsPopup);
+  }
+
+  const toggleCuisinesPopup =() => {
+    setCuisinesPopup(!cuisinesPopup)
+  }
+
   const handleSelectedTime =(time : string) => {
     setSelectedTime(time);
     setTimePopup(!timePopup);
@@ -22,6 +32,15 @@ const Sidebar = () => {
 
   const removeSelectedTime = () => {
     setSelectedTime(null);
+  }
+
+  const handleSelectedCuisine =(cuisine :string) => {
+    setSelectedCuisine(cuisine);
+    setCuisinesPopup(!cuisinesPopup);
+  }
+
+  const removeSelectedCuisine = () => {
+    setSelectedCuisine(null);
   }
 
   return (
@@ -35,15 +54,24 @@ const Sidebar = () => {
         
         {dropdown && (
         <div className='dropdown-container'>
-          <div className='dropdown'><Link to="#"><img id="filterarrow" src='images/filterarrow.svg' />Ingredients</Link></div>
+          <div className='dropdown' onClick={toggleIngredientsPopup}><Link to="#"><img id="filterarrow" src='images/filterarrow.svg' />Ingredients</Link></div>
+            {ingredientsPopup && (
             <div className='ingredientsmenu'>
-              <input className='ingredientssearch' placeholder='Search'></input>
+              <input className='dropdownsearch' placeholder='Search'></input>
+              <div className='ingredientspopup'>Tomato</div>
+              <div className='ingredientspopup'>Chicken</div>
+              <div className='ingredientspopup'>Lettuce</div>
+              <div className='ingredientspopup'>Beef</div>
+              <div className='ingredientspopup'>Fish</div>
+              <div className='ingredientspopup'>Onion</div>
             </div>
+          )}  
+          
           <div className='dropdown' onClick={toggleTimePopup}><Link to="#"><img id="filterarrow" src='images/filterarrow.svg' />Estimated Time</Link></div>
           
           {selectedTime && (
             <div className='selectedtime'>
-              <span className='selectedtime'>{selectedTime}</span>
+              <span className='selectedtimetext'>{selectedTime}</span>
               <button className='remove-time' onClick={removeSelectedTime}>×</button>
             </div>
           )}
@@ -56,10 +84,20 @@ const Sidebar = () => {
             <div className='timepopup' onClick={() => handleSelectedTime('<60 minutes')}><Link to="#">&lt;60 minutes</Link></div>
             </div>
             )}
-          <div className='dropdown'><Link to="#"><img id="filterarrow" src='images/filterarrow.svg' />Cuisine</Link></div>
+
+
+          <div className='dropdown' onClick={toggleCuisinesPopup}><Link to="#"><img id="filterarrow" src='images/filterarrow.svg' />Cuisine</Link></div>
+          {cuisinesPopup && (
             <div className='cuisinesmenu'>
             <input className='dropdownsearch' placeholder='Search'></input>
+            <div className='cuisinepopup'>American</div>
+            <div className='cuisinepopup'>Chinese</div>
+            <div className='cuisinepopup'>Indian</div>
+            <div className='cuisinepopup'>Japanese</div>
+            <div className='cuisinepopup'>Korean</div>
+            <div className='cuisinepopup'>Mexican</div>
             </div>
+          )}
         </div>
     )}
         </div>
