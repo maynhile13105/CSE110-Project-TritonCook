49fdf6eb7c42ad248bce63905b9b02e1bca4c438
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-22 06:07:41 -0800

    updated favorite endpoints to take tokens

--- server/src/endpoints/favorite-endpoints.ts
+++ server/src/endpoints/favorite-endpoints.ts
@@ -2,24 +2,24 @@ import { Database } from "sqlite";
 import { addFavoriteRecipe, checkIsFavoriteRecipe, deleteFavoriteRecipe, getFavoriteRecipes } from "../utils/favorite-utils";
 import { Request, Response } from "express";
 
-export function createFavoriteRecipesEndpoints(app: any, db: Database){
-    app.post("/favorite", (req: Request, res: Response) => {
-        console.log("Received request for displayed recipes");
-        addFavoriteRecipe(req, res, db);
-    });
+export function createFavoriteRecipesEndpoints(app: any, db: Database) {
+  app.post("/favorite", (req: Request, res: Response) => {
+    console.log("Received request for displayed recipes");
+    addFavoriteRecipe(req, res, db);
+  });
 
-    app.delete("/favorite/:userID/:recipeID", (req: Request, res: Response) => {
-        console.log("Received request for displayed recipes");
-        deleteFavoriteRecipe(req, res, db);
-    });
+  app.delete("/favorite/:userID/:recipeID", (req: Request, res: Response) => {
+    console.log("Received request for displayed recipes");
+    deleteFavoriteRecipe(req, res, db);
+  });
 
-    app.get("/favorite", (req: Request, res: Response) => {
-        console.log("Received request for displayed recipes");
-        getFavoriteRecipes(req, res, db);
-    });
+  app.get("/favorite", (req: Request, res: Response) => {
+    console.log("Received request for displayed recipes");
+    getFavoriteRecipes(req, res, db);
+  });
 
-    app.get("/favorite/check/:userID/:recipeID", (req: Request, res: Response) => {
-        console.log("Received request for displayed recipes");
-        checkIsFavoriteRecipe(req, res, db);
-    })
+  app.get("/favorite/check/:userID/:recipeID", (req: Request, res: Response) => {
+    console.log("Received request for displayed recipes");
+    checkIsFavoriteRecipe(req, res, db);
+  })
 }
