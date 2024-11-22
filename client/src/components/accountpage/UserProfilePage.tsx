7cf6cc4480d920f89659459b180d249b684e63d8
Xiaoxiang(Anna) Ma <xim006@ucsd.edu>
2024-11-22 03:52:07 -0800

    basic form of account post

--- client/src/components/accountpage/UserProfilePage.tsx
+++ client/src/components/accountpage/UserProfilePage.tsx
@@ -2,11 +2,60 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./UserProfilePage.css";
 
+// SVG for IconImage
+const IconImage = () => (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M44 6H4L20 24.92V38L28 42V24.92L44 6Z" stroke="#1E1E1E" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+  
+  // SVG for IconEdit3
+  const IconEdit3 = () => (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M44 6H4L20 24.92V38L28 42V24.92L44 6Z" stroke="#1E1E1E" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+
 const UserProfilePage = () => {
     return (
       <div className="page_style">
-        <h1>User Profile</h1>
-        {/* Display user profile information here */}
+        <div className="profile-block">
+            {/* Centered Icon */}
+            <div className="pic_icon">
+                <IconImage />
+            </div>
+
+            {/* Right Corner Edit Icon */}
+            <div className="pen_icon">
+                <IconEdit3 />
+            </div>
+        </div>
+
+        {/* Second Block */}
+        <div className="info-block">
+            {/* Username */}
+            <div className="username">UserName</div>
+
+            {/* Title */}
+            <div className="info-title">Title</div>
+
+            {/* List Content */}
+            <ul className="info-list">
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+            </ul>
+
+            <button className="see-more-button">... see more</button>
+
+            {/* Image Display Area */}
+            <div className="image-display">
+                <p>
+                    image placeholder
+                </p>
+            </div>
+        </div>
+
       </div>
     );
   };
