ffb572e02312e913afa3fb80d0f85a9d8de0264b
Xiaoxiang(Anna) Ma <xim006@ucsd.edu>
2024-11-22 14:37:28 -0800

    info block finish

--- client/src/components/accountpage/UserInfoPage.tsx
+++ client/src/components/accountpage/UserInfoPage.tsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import "./UserInfoPage.css";
 
 const UserInfoPage = () => {
+    const userInfoData = [
+        { label: "Username", info: "Example1" },
+        { label: "Password", info: "*****" },
+        { label: "Email", info: "students@ucsd.edu" },
+        { label: "DOB", info: "MM/DD/YYYY" },
+        { label: "Ethic", info: "Asian/Asian American" },
+        { label: "Phone", info: "+1(123)-456-7890" },
+    ];
+
     return (
         <div className="page_style">
             {/* Center Top Button */}
@@ -10,13 +19,13 @@ const UserInfoPage = () => {
 
             {/* Blocks Below Button */}
             <div className="blocks-container">
-                {["Username", "Password", "Email", "DOB", "Ethic", "Phone"].map((text, index) => (
+                {userInfoData.map((data, index) => (
                     <div className="block" key={index}>
                         <div className="block-text-container">
-                            <span className="block-text">{text}</span>
+                            <span className="block-text">{data.label}</span>
                         </div>
                         <div className="block-info-container">
-                            <span className="block-info">{text}</span>
+                            <span className="block-info">{data.info}</span>
                         </div>
                     </div>
                 ))}
