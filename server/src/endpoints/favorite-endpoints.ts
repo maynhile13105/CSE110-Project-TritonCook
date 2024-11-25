4e752e9a853da3c0d675ca50af1dffa502535562
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2024-11-25 11:18:27 -0800

    added some test cases for favorite

--- server/src/endpoints/favorite-endpoints.ts
+++ server/src/endpoints/favorite-endpoints.ts
@@ -4,22 +4,22 @@ import { Request, Response } from "express";
 
 export function createFavoriteRecipesEndpoints(app: any, db: Database) {
   app.post("/favorite", (req: Request, res: Response) => {
-    console.log("Received request for displayed recipes");
+    console.log("Received request for adding favorite recipes");
     addFavoriteRecipe(req, res, db);
   });
 
-  app.delete("/favorite/:userID/:recipeID", (req: Request, res: Response) => {
-    console.log("Received request for displayed recipes");
+  app.delete("/favorite", (req: Request, res: Response) => {
+    console.log("Received request for deleting favorite recipes");
     deleteFavoriteRecipe(req, res, db);
   });
 
   app.get("/favorite", (req: Request, res: Response) => {
-    console.log("Received request for displayed recipes");
+    console.log("Received request for getting favorite recipes");
     getFavoriteRecipes(req, res, db);
   });
 
-  app.get("/favorite/check/:userID/:recipeID", (req: Request, res: Response) => {
-    console.log("Received request for displayed recipes");
-    checkIsFavoriteRecipe(req, res, db);
-  })
+  // app.get("/favorite/check/:userID/:recipeID", (req: Request, res: Response) => {
+  //   console.log("Received request for checking favorite recipes");
+  //   checkIsFavoriteRecipe(req, res, db);
+  // })
 }
