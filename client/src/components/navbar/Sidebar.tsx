a73ae59e3365e7bf6f2f714304740f503dbd2ba2
ribaltazar <ribaltazar@ucsd.edu>
2024-12-02 17:59:19 -0800

    sidebar profile icon update

--- client/src/components/navbar/Sidebar.tsx
+++ client/src/components/navbar/Sidebar.tsx
@@ -109,7 +109,7 @@ const Sidebar = () => {
     <div>
       <div className='sidebar-container' role='button'>
         <div className='mainbuttons-container'>
-        <Link to={`/profile/${userProfile.name}`} data-testid='profileButton'><div className='profilebutton'><div className="profile-content"><UserIcon data-testid='UserIcon'/><span>Profile</span></div></div></Link>
+        <Link to={`/profile/${userProfile.name}`} data-testid='profileButton'><div className='profilebutton' data-testid='UserIcon'><div className="profile-content">{userProfile?.picture? (<img className='profile' src={userProfile.picture} alt="user-avatar"/>):(<img src="/images/profile.svg" alt="defaultprofile" className="defaultprofile" />)}<span>Profile</span></div></div></Link>
         <Link to="/home/favorite"><div className='favoritebutton' data-testid='FavoriteButton'><img id="saved-icon" src='/images/favorite.svg' alt='favoriteIcon'/>Favorites</div></Link>
         <Link to="#"><div className='filterbutton' onClick={toggleDropdown} data-testid="filter-button"><img id="filter-icon" src='/images/filter.svg' alt='filterIcon'/>Filter</div></Link>
         
